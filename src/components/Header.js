d6b7280119e8ab0bc23e03c94f456eb2da5f5375
sanjeev473 <guptasanjeev473@gmail.com>
2022-06-25 10:24:41 +0530

    add double s

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="">Customer Rewards</a>
+                <a className="navbar-brand" href="">Customer Rewardss</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
